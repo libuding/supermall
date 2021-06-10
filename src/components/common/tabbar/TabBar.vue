6242767e8c56fa3b5acdfe4e1bdae170d3c2728c
lqh <1778328739@qq.com>
2021-06-10 09:59:03 +0800

    首页

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+    <!-- <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/email.svg" alt />
+      首页
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/email.svg" alt />
+      分类
+    </div>
+    <div class="tab-bar-item">
+      购物车
+      <img src="../../assets/img/tabbar/email.svg" alt />
+    </div>
+    <div class="tab-bar-item">
+      我的
+      <img src="../../assets/img/tabbar/email.svg" alt />
+    </div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar"
+};
+</script>
+
+<style>
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0px -1px 10px rgba(100, 100, 100, 0.8);
+}
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <!-- <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>-->
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+  <!-- <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/email.svg" alt />
+      首页
+    </div>
+    <div class="tab-bar-item">
+      <img src="../../assets/img/tabbar/email.svg" alt />
+      分类
+    </div>
+    <div class="tab-bar-item">
+      购物车
+      <img src="../../assets/img/tabbar/email.svg" alt />
+    </div>
+    <div class="tab-bar-item">
+      我的
+      <img src="../../assets/img/tabbar/email.svg" alt />
+  </div>-->
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: "red"
+    }
+  },
+  data() {
+    return {
+      //   isActive: true
+      //   path: "/home"
+    };
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) != -1;
+    },
+    activeStyle() {
+      return this.isActive ? { color: this.activeColor } : {};
+    }
+  },
+  methods: {
+    itemClick() {
+      //   console.log("你好");
+      console.log("this.path");
+      console.log(this.path);
+      this.$router.replace(this.path);
+    }
+  }
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  /* margin-bottom: 0.5px; */
+}
+/* .active {
+  color: red;
+} */
+</style>
